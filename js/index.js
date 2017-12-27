b72ca91f2e36d6549df7c64406f61beff570ca0e
w103.keren <keren.w@gmail.com>
2017-12-27 13:25:32 +0200

    basic skeleton

---
+++ js/index.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    // document.getElementsByClassName("hide-title")[0].classList.remove("hide-title");
+    // document.getElementsByClassName("hide-subtitle")[0].titleLeft.classList.remove("hide-subtitle");
+}
