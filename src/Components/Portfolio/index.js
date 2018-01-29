0daa965ffe5814d5a1f79d47d6c44ebd9d354c1b
keren <keren.w@gmail.com>
2018-01-29 18:47:39 +0200

    pushing new version online...

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./style.css";
 import Project from "../Project";
 import projects from "../../static/json/projects.json";
 import SectionTitle from "../SectionTitle";
 
-export default () => {
-  return (
-    <div id={styles.portfolio}>
-      <SectionTitle>.Portfolio</SectionTitle>
-      <div className={styles["vertical-center-section"]}>
-        <div className={styles["projects-container"]}>
-          <div className={styles["container-sizer"]}>
-            <ul className={styles["nav-projects"]}>
-              {projects.map((item, index) => <li key={index} className={styles["project-link"]}>{item.title}</li>)}
-            </ul>
-            <div className={styles["projects-preview"]}>
-            {projects.map((item, index) => <Project key={index}></Project>)}
+export default class Portfolio extends Component {
+  constructor() {
+    super()
+    this.state = {currentlyViewed: 1}
+  }
+
+  changeViewedProject(projectIndex) {
+    let {currentlyViewed} = this.state;
+    // console.log(projectIndex)
+    console.log(currentlyViewed)
+    if (projectIndex === currentlyViewed) {
+      return;
+    } else {
+      return this.setState({currentlyViewed: projectIndex})
+    }
+  }
+
+  render() {
+    let {currentlyViewed} = this.state;
+    return (
+      <div id={styles.portfolio}>
+        <SectionTitle>.Portfolio</SectionTitle>
+        <div className={styles["section-vertical-centerer"]}>
+          <div className={styles["view-centerer"]}>
+            <div className={styles["view-sizer"]}>
+              <ul className={styles["nav-projects"]}>
+                {projects.map((item, index) => (
+                  <li key={index} className={styles["project-link"]} onClick={() => this.changeViewedProject(item.id)}>
+                    {item.title}
+                  </li>
+                ))}
+              </ul>
+              <div className={styles["projects-preview"]}>
+                {projects.map((item, index) => {
+                  return <Project key={index} item={item} isViewed={currentlyViewed === item.id}/>}
+                )}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-{/* <Project key={item.id} project={item} /> */}
+    );
+  }
+}
--- src/Components/Project/index.js
+++ src/Components/Project/index.js
@@ -1,52 +1,56 @@
-import React, { Component } from 'react';
-import styles from './style.css';
-import Tag from '../Tag';
+import React, { Component } from "react";
+import styles from "./style.css";
+import Tag from "../Tag";
 
-export default class project extends Component {
-    constructor() {
-        super();
-        this.state = {showPreview: false}
-        this.toggleProjectPreview = this.toggleProjectPreview.bind(this);
-        this.projectPreview = this.projectPreview.bind(this);
-    }
+export default class Project extends Component {
+  constructor() {
+    super();
+    this.state = { showPreview: false };
+    this.toggleProjectPreview = this.toggleProjectPreview.bind(this);
+    this.projectPreview = this.projectPreview.bind(this);
+  }
 
-    toggleProjectPreview() {
-        let currentlyPreviewing =  this.state.showPreview;
-        this.setState({showPreview: !currentlyPreviewing})
-    }
+  toggleProjectPreview() {
+    let currentlyPreviewing = this.state.showPreview;
+    this.setState({ showPreview: !currentlyPreviewing });
+  }
 
-    projectPreview() {
-        let {project} = this.props;
-        return (
-            <div className={styles['project-modal-container']}>
-                <div className={styles['project-modal']}>
-                    <a href={project['link-address']}>
-                        <h3 className={styles['project-title']}>{project.title}</h3>
-                    </a>
-                    <p>{project.description}</p>
-                    <div className={styles['tags-container']}>{displayTags(project.technologies)}</div>
-                    <p><span>Go to repository</span></p>
-                </div>
-            </div>
-        )
-    }
-    render() {
-        let {showPreview} = this.state;
-        return  (
-            <div className={styles['p-item']} onClick={this.toggleProjectPreview}>
-              <ProjectTitle project={this.props.project}/>
-              {showPreview && this.projectPreview()}
-            </div>
-        )
-    }
-}
-
-const ProjectTitle = ({project}) => {
+  projectPreview() {
+    let { item } = this.props;
     return (
-        <h3 className={styles['project-title']}>ada</h3>
-    )
+      <div className={styles["project-modal-container"]}>
+        <div className={styles["project-modal"]}>
+          <a href={item["link-address"]}>
+            <h3 className={styles["project-title"]}>{item.title}</h3>
+          </a>
+          <p>{item.description}</p>
+          <div className={styles["tags-container"]}>
+            {displayTags(item.technologies)}
+          </div>
+          <p>
+            <span>Go to repository</span>
+          </p>
+        </div>
+      </div>
+    );
+  }
+  render() {
+      let { item, isViewed } = this.props;
+
+      if (isViewed) {
+          return <div className={styles["projects-explained"]} name={item.title}>
+          <ProjectTitle project={this.props.item} />
+        </div>
+      } else {
+          return null;
+      }
+  }
 }
 
-const displayTags = (tagsArray) => {
-    return tagsArray.map((tag, index) => <Tag key={index} tagContent={tag}></Tag>)
-}
+const ProjectTitle = ({ project }) => {
+  return <h3 className={styles["project-title"]}>{project.title}</h3>;
+};
+
+const displayTags = tagsArray => {
+  return tagsArray.map((tag, index) => <Tag key={index} tagContent={tag} />);
+};
